d3daac402114c2b74e9d6af0f647ceb599c7602a
Luis Miguel Morillas <morillas@gmail.com>
2017-03-21 10:49:31 +0100

    Initial commit

---
+++ pictospartos/home/models.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+from __future__ import absolute_import, unicode_literals
+
+from django.db import models
+
+from wagtail.wagtailcore.models import Page
+
+from wagtail.wagtailadmin.edit_handlers import (
+    FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel, PageChooserPanel, StreamFieldPanel,
+)
+
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+
+class HomePage(Page):
+    pass
+
+
+
+class Pictograma(models.Model):
+    """
+
+    """
+    nombre = models.CharField("Nombre del picto", max_length=254, blank=True)
+    picto = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Picto'
+    )
+    descripcion = models.TextField(
+    	help_text="descripción del picto",
+        blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('introduction', classname="full"),
+        ImageChooserPanel('image'),
+        FieldPanel('descripcion'),
+    ]

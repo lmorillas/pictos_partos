8ca0532604fd0b872a235ff274715b017ff449ce
lmorillas <morillas@gmail.com>
2017-11-25 22:44:02 +0100

    contacto

--- home/models.py
+++ home/models.py
@@ -40,6 +40,7 @@ from wagtail.wagtailcore.blocks import (
     CharBlock, ChoiceBlock, ListBlock, RichTextBlock, StreamBlock, StructBlock, TextBlock,
 )
 from wagtail.wagtailsearch import index
+from wagtail.wagtailforms.models import AbstractEmailForm, AbstractFormField
 
 class HomePage(Page):
     carousel = StreamField(CarouselBlock(), null=True, blank=True)
@@ -328,4 +329,43 @@ class PaginaEstandar(Page):
         FieldPanel('introduction', classname="full"),
         StreamFieldPanel('body'),
         ImageChooserPanel('image'),
+    ]
+
+class FormField(AbstractFormField):
+    """
+    Wagtailforms is a module to introduce simple forms on a Wagtail site. It
+    isn't intended as a replacement to Django's form support but as a quick way
+    to generate a general purpose data-collection form or contact form
+    without having to write code. We use it on the site for a contact form. You
+    can read more about Wagtail forms at:
+    http://docs.wagtail.io/en/latest/reference/contrib/forms/index.html
+    """
+    page = ParentalKey('FormPage', related_name='form_fields')
+
+
+class FormPage(AbstractEmailForm):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    body = StreamField(BaseStreamBlock())
+    thank_you_text = RichTextField(blank=True)
+
+    # Note how we include the FormField object via an InlinePanel using the
+    # related_name value
+    content_panels = AbstractEmailForm.content_panels + [
+        ImageChooserPanel('image'),
+        StreamFieldPanel('body'),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text', classname="full"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
     ]
--- pictopartos/settings/base.py
+++ pictopartos/settings/base.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'cuser',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -156,3 +157,5 @@ CACHES = {
         }
     }
 }
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
